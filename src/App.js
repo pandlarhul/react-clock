1ee08257603528b130f29a015d2884fb8bdac156
Rahul Rohilla <rahul.kumar13@mountblue.tech>
2020-09-12 13:12:24 +0530

    rendered display component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header'
+import Display from './components/Display'
 
 function App() {
   return (
@@ -9,6 +8,8 @@ function App() {
       <header>
         <Header />
       </header>
+
+      <Display />
     </div>
   );
 }
