b496a0336ee838085f7de5416b03c8e8ce44522b
Rahul Rohilla <rahul.kumar13@mountblue.tech>
2020-09-12 13:07:19 +0530

    imported switch and clock components and rendered

---
+++ src/components/Display.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import 'font-awesome/css/font-awesome.min.css';
+import './Display.css'
+import Switch from './Switch';
+import Clock from './Clock'
+
+
+class Display extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isToggleOn : false
+    }
+  }
+
+  handleChange = (event) => {
+    console.log("calling in parent");
+    this.setState((state)=>{
+      return {isToggleOn : !state.isToggleOn};
+    });
+
+  }
+
+  render(){
+    return (
+      <div className="display">
+        <Switch 
+        onSwitchChange={this.handleChange}
+        isToggleOn={this.state.isToggleOn}
+        />
+
+        <Clock isToggleOn={this.state.isToggleOn}/>
+      </div>
+      )
+  }
+
+}
+
+export default Display;
